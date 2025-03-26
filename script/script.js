ad50bf3ca573a1ce93585a7004ce24f92e4782d6
Nathan2012man <natopokuboateng@yahoo.com>
2025-03-26 21:25:45 +0000

    first commit

---
+++ script/script.js
@@ -0,0 +1,35 @@
+let cart = [];
+console.log(products);
+function displayProducts() {
+    console.log(products);
+    console.log("Displaying products...");
+    let productsContainer = 
+    document.getElementById("products");
+  
+    productsContainer.innerHTML = "";
+      //Clear previous content 
+      products.forEach(product => {
+      let productDiv = 
+      document.createElement("div"); 
+      productDiv.classList.add("product"); // Ensure the "product" class is styled in your CSS
+       productDiv.innerHTML = `
+       <div id="product-details">
+       <div id="product-image">
+        <img src="${product.image}" alt="${product.name}" height="300" width="300">
+        </div>
+        <div id="product-info">
+        <h3>${product.name}</h3>
+        <p>${product.desc}</P>
+        <p>Price: $${product.price}</p>
+        <button onclick="addToCart(${product.id})">Add to Cart</button>
+        </div>
+        </div>
+       `;
+         productsContainer.appendChild(productDiv); 
+         });
+        };
+         
+        document.addEventListener("DOMContentLoaded", () => {
+            displayProducts();
+        });
+        
